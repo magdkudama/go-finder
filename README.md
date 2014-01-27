6d7ff8c5d35af8f48945d4e46292ff2b43947e6f
magdkudama <magdkudama@gmail.com>
2014-01-27 23:34:09 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+"Finder" is a simple library to find files in your filesystem (as it's name suggests), using pure Go (no external dependencies).
+
+Please, don't use it until I write some tests (this library is just my playgound). But feel free to help me improve the library, as it's my very first Go code.
+
+## Quick Start
+
+```go
+package main
+
+import (
+	"github.com/magdkudama/finder"
+	"fmt"
+)
+
+func main() {
+	results := finder.
+		Create("/my/path/").
+		Depth(1).
+		Name(".go").
+		NotName(".html")
+		Get()
+
+	for _, element := range results {
+		fmt.Println(element.Name())
+	}
+}
+```
---
+++ finder.go
@@ -0,0 +1,144 @@
+package finder
+
+import (
+	"os"
+	"regexp"
+	"path/filepath"
+	"strings"
+	"errors"
+	"io/ioutil"
+)
+
+var (
+	ErrInvalidArgument = errors.New("finder: invalid argument")
+	ErrNotDir = errors.New("finder: not dir")
+)
+
+type Finder struct {
+	namesLike, namesNotLike	[]*regexp.Regexp
+	in						string
+	depth					int
+}
+
+type FinderResult struct {
+	finder 	Finder
+	err 	error
+}
+
+func checkName(names []*regexp.Regexp, elementName string, hasToMatch bool, add *bool) {
+	if *add == true {
+		for _,r := range names {
+			if r.MatchString(elementName) == (!hasToMatch) {
+				*add = false
+				break
+			}
+		}
+	}
+}
+
+func isValidDepth(path string, maxDepth int, baseDepth int) bool {
+	pathDepth := depth(path)
+	if maxDepth > -1 {
+		maxDepth += baseDepth
+		if pathDepth > maxDepth {
+			return false
+		}
+	}
+
+	return true
+}
+
+func depth(path string) int {
+	separator := string(os.PathSeparator)
+	absolutePath,_ := filepath.Abs(path)
+	return len(strings.Split(absolutePath, separator))
+}
+
+func readDirectory(path string, depth int, baseDepth int, f Finder) []os.FileInfo {
+	var items []os.FileInfo
+
+	var elements,err = ioutil.ReadDir(path)
+	if(err == nil) {
+		for _,element := range elements {
+			if element.IsDir() {
+				newPath := path + string(os.PathSeparator) + element.Name()
+				if isValidDepth(newPath, f.depth, baseDepth) {
+					recElements := readDirectory(newPath, depth + 1, baseDepth, f)
+					for _,recElement := range recElements {
+						items = append(items, recElement)
+					}
+				}
+			} else {
+				add := true
+				checkName(f.namesLike, element.Name(), true, &add)
+				checkName(f.namesNotLike, element.Name(), true, &add)
+				if add {
+					items = append(items, element)
+				}
+			}
+		}
+	}
+
+	return items
+}
+
+func Create(path string) FinderResult {
+	path = strings.Trim(path, " ")
+	if path[(len(path) - 1):] == "/" {
+		path = path[:(len(path) - 1)]
+	}
+
+	baseFinder := FinderResult {
+		finder: Finder { in: path },
+		err: nil,
+	}
+
+	fi,err := os.Lstat(path)
+	if err != nil {
+		baseFinder.err = err
+	}
+
+	if !fi.IsDir() {
+		baseFinder.err = ErrNotDir
+	}
+
+	return baseFinder
+}
+
+func (f FinderResult) Depth(depth int) FinderResult {
+	if depth < 0 {
+		f.err = ErrInvalidArgument
+	} else {
+		f.finder.depth = depth
+	}
+
+	return f
+}
+
+func (f FinderResult) NotName(pattern string) FinderResult {
+	regexp, e := regexp.Compile(pattern)
+
+	if e != nil {
+		f.err = e
+	} else {
+		f.finder.namesNotLike = append(f.finder.namesNotLike, regexp)
+	}
+
+	return f
+}
+
+func (f FinderResult) Name(pattern string) FinderResult {
+	regexp, e := regexp.Compile(pattern)
+
+	if e != nil {
+		f.err = e
+	} else {
+		f.finder.namesLike = append(f.finder.namesNotLike, regexp)
+	}
+
+	return f
+}
+
+func (f FinderResult) Get() []os.FileInfo {
+	return readDirectory(f.finder.in, 0, depth(f.finder.in), f.finder)
+}
